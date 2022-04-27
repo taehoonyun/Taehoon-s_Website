ceaa5f40db7a386b360eefcbb70ed2459a166028
taehoonyun <59932429+taehoonyun@users.noreply.github.com>
2022-04-27 12:29:04 +0900

    Delete apple directory

--- apple/background.js
+++
@@ -1,8 +0,0 @@
-const images = ["01.jpg","02.jpg","03.jpg"];
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImg = document.createElement("img");
-bgImg.src =`img/${chosenImage}`;
-
-document.body.appendChild(bgImg);
