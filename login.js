cd1ea70fc5eab728ac960bcec392bad1281c0c96
taehoonyun <mn9069@naver.com>
2022-06-30 09:52:49 +0900

    design

--- login.js
+++ login.js
@@ -1,6 +1,8 @@
 const login = document.querySelector("#loginform");
 const loginBtn = document.querySelector("#login");
+const login_func = document.querySelector("#login_func");
 const greeting = document.querySelector("#greeting");
+const main_contents = document.querySelector("#main_contents");
 const HIDDENCLASS = "hidden";
 
 function onLoginBtnClick(event) {
@@ -8,20 +10,19 @@ function onLoginBtnClick(event) {
   const ID = loginBtn.value;
   console.log(ID);
   localStorage.setItem("ID", ID);
-  login.classList.add(HIDDENCLASS);
+  login_func.classList.add(HIDDENCLASS);
   paintGreeting(ID);
 }
 const getID = localStorage.getItem("ID");
 
 function paintGreeting(getID) {
-  greeting.classList.remove(HIDDENCLASS);
+  main_contents.classList.remove(HIDDENCLASS);
   greeting.innerHTML = "Hello " + getID + "!!";
 }
 
 if (getID !== null) {
   paintGreeting(getID);
 } else {
-  //   greeting.classList.add(HIDDENCLASS);
-  login.classList.remove(HIDDENCLASS);
+  login_func.classList.remove(HIDDENCLASS);
   login.addEventListener("submit", onLoginBtnClick);
 }
